59d0f7422faabd128de267992ae0ef8da6eb7b42
Fer Torres <fertorresmx@gmail.com>
2022-05-03 23:53:03 -0500

    Testing first route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return 'Pantalla login';
+});
+
+Route::get('/test_welcome', function () {
     return view('welcome');
 });
+
