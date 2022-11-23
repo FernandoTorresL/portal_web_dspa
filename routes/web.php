e54ce210c1be0e5fc28d16c002a9ccf2a73cfe83
Fer Torres <fertorresmx@gmail.com>
2022-11-23 16:49:45 -0600

    Add code from class 'Views'

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,31 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Example: http://localhost:8000
 Route::get('/', function () {
-    return 'Ruta home';
+    return view('home');
 });
 
 // Example: http://localhost:8000/blog
 Route::get('blog', function () {
-    return 'Listado de publicaciones';
+    // Consulta a base de datos
+    $posts = [
+        ['id' => 1, 'title' => 'PHP',       'slug' => 'php'],
+        ['id' => 2, 'title' => 'Laravel',   'slug' => 'laravel'],
+    ];
+    return view('blog', ['posts' => $posts]);
 });
 
 // Example: http://localhost:8000/blog/plaravel
 Route::get('blog/{slug}', function ($slug) {
     // Consulta a base de datos
-    return $slug;
+    $post = $slug;
+    return view('post', ['post' => $post]);
 });
 
 // Example: http://localhost:8000/buscar?query=Lo_que_estoy_buscando
-Route::get('buscar', function (Request $request) {
+/* Route::get('buscar', function (Request $request) {
     // Consulta a base de datos
     return $request->all();
-});
+}); */
+
