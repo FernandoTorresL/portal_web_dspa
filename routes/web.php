a1c59a680810aa83290715ae58951b1842e18046
Fer Torres <fertorresmx@gmail.com>
2022-11-23 15:09:20 -0600

    Add code from class 'Routes'

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,26 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+// Example: http://localhost:8000
 Route::get('/', function () {
-    return view('welcome');
+    return 'Ruta home';
+});
+
+// Example: http://localhost:8000/blog
+Route::get('blog', function () {
+    return 'Listado de publicaciones';
+});
+
+// Example: http://localhost:8000/blog/plaravel
+Route::get('blog/{slug}', function ($slug) {
+    // Consulta a base de datos
+    return $slug;
+});
+
+// Example: http://localhost:8000/buscar?query=Lo_que_estoy_buscando
+Route::get('buscar', function (Request $request) {
+    // Consulta a base de datos
+    return $request->all();
 });
