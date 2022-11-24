c6062d26742eea3191b8db8f19500efe9e203c13
Fer Torres <fertorresmx@gmail.com>
2022-11-24 09:42:15 -0600

    Add code from class 'Controllers'
    - Add a Controller
    - Use the controller on routes
    - Group routes for a controller

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,24 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-// Example: http://localhost:8000/blog
-Route::get('blog', function () {
-    // Consulta a base de datos
-    $posts = [
-        ['id' => 1, 'title' => 'PHP',       'slug' => 'php'],
-        ['id' => 2, 'title' => 'Laravel',   'slug' => 'laravel'],
-    ];
-    return view('blog', ['posts' => $posts]);
-})->name('blog');
-
-// Example: http://localhost:8000/blog/plaravel
-Route::get('blog/{slug}', function ($slug) {
-    // Consulta a base de datos
-    $post = $slug;
-    return view('post', ['post' => $post]);
-})->name('post');
 
 // Example: http://localhost:8000/buscar?query=Lo_que_estoy_buscando
 /* Route::get('buscar', function (Request $request) {
     // Consulta a base de datos
     return $request->all();
 }); */
+
+// Routes usando PageController en cada línea:
+/*
+Route::get('/',             [PageController::class,   'home'])->name('home');
+Route::get('blog',          [PageController::class,   'blog'])->name('blog');
+Route::get('blog/{slug}',   [PageController::class,   'post'])->name('post');
+*/
+
+// Para no repetir el controlador, se incluyen en un grupo:
+Route::controller(PageController::class)->group(function () {
+    Route::get('/',             'home')->name('home');
+    Route::get('blog',          'blog')->name('blog');
+    Route::get('blog/{slug}',   'post')->name('post');
+});
