1278056d58c15de12053c238bb949ecb0743e90c
Fer Torres <fertorresmx@gmail.com>
2022-11-29 12:29:13 -0600

    Add code from class 'Registros duplicados'

--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -6,6 +6,13 @@
     value="{{ old('title', $post->title) }}"
 >
 
+<label class="uppercase text-gray-700 text-xs">Slug</label>
+<span class="text-xs text-red-600">@error('slug') {{ $message }} @enderror</span>
+
+<input type="text" name="slug" class="rounded border-gray-200 w-full mb-4"
+    value="{{ old('slug', $post->slug) }}"
+>
+
 <label class="uppercase text-gray-700 text-xs">Contenido</label>
 <span class="text-xs text-red-600">@error('body') {{ $message }} @enderror</span>
 <textarea name="body" rows="5" class="rounded border-gray-200 w-full mb-4">{{ old('body', $post->body) }}</textarea>
