726140340d9e9e37d2a360297e09668693885f02
Fernando Torres <fertorresmx@gmail.com>
2022-11-30 12:56:59 -0600

    Update template.blade.php
    Testing automatic deploy on digitalocean with this minor change

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Proyecto Web</title>
+    <title>{{ env('APP_NAME') }}</title>
 
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
